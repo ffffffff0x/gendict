7784fc53064b1887b9dd0cdec7d14069eb999f32
r0fus0d <d2hwakh7bs5e@protonmail.com>
2022-09-21 19:22:09 +0800

    更改默认输出文件名称

--- README.md
+++ README.md
@@ -43,6 +43,9 @@ CONFIGURATIONS:
 # 对已知员工列表进行字典生成,指定输出到 /tmp/out.txt 中
 ./gendict -name test.txt -nameout /tmp/out.txt
 
+# 提供一个已知用户名列表
+./gendict -list username.txt
+
 # 提供一个已知用户名列表,使用自定义后缀列表
 ./gendict -list username.txt -suffix passuffix.txt
 ```
--- mod/flag.go
+++ mod/flag.go
@@ -22,9 +22,9 @@ func init() {
 	flag.StringVar(&Org, "org", "", "组织名称列表")
 	flag.StringVar(&Name, "name", "", "用户名称列表")
 	flag.StringVar(&List, "list", "", "批量拼接后缀")
-	flag.StringVar(&Orgout, "orgout", "output/org.txt", "组织名称结果输出")
-	flag.StringVar(&Nameout, "nameout", "output/name.txt", "用户名称结果输出")
-	flag.StringVar(&Listout, "listout", "output/list.txt", "批量拼接结果输出")
+	flag.StringVar(&Orgout, "orgout", "orgresult.txt", "组织名称结果输出")
+	flag.StringVar(&Nameout, "nameout", "nameresult.txt", "用户名称结果输出")
+	flag.StringVar(&Listout, "listout", "listresult.txt", "批量拼接结果输出")
 	flag.StringVar(&Suffix, "suffix", "suffix.txt", "自定义后缀")
 
 }
